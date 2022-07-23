781136b2b2b16c0dac808ff69e6c57831416f708
Nishkarsh <nishkarsh2912@gmail.com>
2022-07-23 17:52:56 +0530

    add test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-let d= new Date()
-let options={
-    weekday:'long',
-    month:'numeric',
-    year:'numeric',
-    // day:'long'
-}
-let displayDate=d.toLocaleDateString('en-US')
-console.log(displayDate);
