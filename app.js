24a327d5def1e2fd3ac480170a6200bcdfaad582
Nishkarsh1606 <ns513@student.london.ac.uk>
2022-07-28 12:06:34 +0530

    add post request

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require('ejs');
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:true}))
 app.set("view engine", "ejs")
+let newTasksArray=[]
 
 const daysOfWeek=['Monday','Tue','Wed','Thu','Fri','Sat','Sun']
 let message=""
@@ -17,11 +18,28 @@ app.get('/',function(request,response){
 //---------logic for building the weekdays--------
 const weekday=new Date()
 let today=daysOfWeek[weekday.getDay()]
+
+//trying toLocalString method
+let options={
+    month:'long',
+    day:'numeric',
+    year:'numeric'
+}
+let today2=weekday.toLocaleDateString('en-UK',options)
+console.log(today2);
+// End------
 let date=weekday.getDay()
 if (date===5 || date===6) {
-    message='Its a weekend'
+    message=`Today is ${today}. It's a weekend!`
 } else {
-    message="its a weekday"
+    message=`Today is ${today}. It's a weekday :(`
 }
-response.render(`list`,{kindOfDay:today,messageForDay:message})
+response.render(`list`,{kindOfDay:today,messageForDay:message,newListItem:newTasksArray})
+})
+
+//Making post requests
+app.post('/',(request,response)=>{
+    let newTasks=request.body.newItem
+    newTasksArray.push(newTasks)
+    response.redirect('/')
 })
