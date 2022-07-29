dc2e16c42ba6371e0f879e7505f197c44468f21d
Nishkarsh <nishkarsh2912@gmail.com>
2022-07-29 13:51:37 +0530

    add work list(1/2)

--- app.js
+++ app.js
@@ -1,68 +1,59 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+let newTasksArray = [];
+let workList = [];
 
-const express=require('express')
-const app=express()
-const bodyParser=require('body-parser')
-const ejs = require('ejs');
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended:true}))
-app.set("view engine", "ejs")
-let newTasksArray=[]
-
-
-const daysOfWeek = ["Monday", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 let message = "";
 
 app.listen(3000, () => {
-    console.log(`server started`);
+  console.log(`server started`);
 });
 
-
-app.get('/',function(request,response){
-//---------logic for building the weekdays--------
-const weekday=new Date()
-let today=daysOfWeek[weekday.getDay()]
-
-//trying toLocalString method
-let options={
-    month:'long',
-    day:'numeric',
-    year:'numeric'
-}
-let today2=weekday.toLocaleDateString('en-UK',options)
-console.log(today2);
-// End------
-let date=weekday.getDay()
-if (date===5 || date===6) {
-    message=`Today is ${today}. It's a weekend!`
-} else {
-    message=`Today is ${today}. It's a weekday :(`
-}
-response.render(`list`,{kindOfDay:today,messageForDay:message,newListItem:newTasksArray})
-})
+const weekday = new Date();
+let today = daysOfWeek[weekday.getDay()];
+console.log(today);
 
 app.get("/", function (request, response) {
-    //---------logic for building the weekdays--------
-    const weekday = new Date();
-    let options = {
-        weekday: "long",
-    };
-    let today = weekday.toLocaleDateString("en-UK", options);
-    let date = weekday.getDay();
-    if (date === 5 || date === 6) {
-        message = "Its a weekend";
-    } else {
-        message = "its a weekday";
-    }
-    response.render(`list`, {
-        kindOfDay: today,
-        messageForDay: message,
-        typeOfDate: date,
-    });
+
+  //trying toLocalString method
+  let options = {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  };
+  let today2 = weekday.toLocaleDateString("en-UK", options);
+  console.log(today2);
+  // End------
+  let date = weekday.getDay();
+  if (date === 5 || date === 6) {
+    message = `Today is ${today}. It's a weekend!`;
+  } else {
+    message = `Today is ${today}. It's a weekday :(`;
+  }
+  response.render(`list`, {
+    listTitle: today,
+    messageForDay: message,
+    newListItem: newTasksArray,
+  });
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", {
+    listTitle: today,
+    messageForDay: `Let's get to work`,
+    newListItem: workList,
+  });
 });
 
 //Making post requests
-app.post('/',(request,response)=>{
-    let newTasks=request.body.newItem
-    newTasksArray.push(newTasks)
-    response.redirect('/')
-})
+app.post("/", (request, response) => {
+  let newTasks = request.body.newItem;
+  newTasksArray.push(newTasks);
+  response.redirect("/");
+});
